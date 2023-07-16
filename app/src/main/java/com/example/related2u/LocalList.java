d65bfedc7a94d4b62b5ebcfeb2c0dadd8b049efe
Jang-Sohyun <111950769+Jang-Sohyun@users.noreply.github.com>
2023-07-16 12:08:58 +0900

    Initial commit

---
+++ app/src/main/java/com/example/related2u/LocalList.java
@@ -0,0 +1,17 @@
+package com.example.related2u;
+
+public class LocalList {
+    private String local;
+
+    public LocalList() {
+
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+}
